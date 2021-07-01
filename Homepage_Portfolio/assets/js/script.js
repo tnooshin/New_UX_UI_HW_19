68e47895007de5e4fe7f9e139a4d6fae4e142d4c
tnooshin <tnooshinux@gmail.com>
2021-07-01 14:57:00 -0700

    Animation in js
    Added animation is in js for homepage, HW #19

---
+++ Homepage_Portfolio/assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$(window).on("load",function(){
+    $("#animation1").fadeIn(3000);
+    $("#animation2").fadeIn(6000);
+    $("#animation3").fadeIn(9000);
+})
+
+
+
+
+})//end ducument ready function/
